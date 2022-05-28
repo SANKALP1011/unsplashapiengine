b6c7f0aaa27a5994581570ccfbee90760a84d240
SANKALP1011 <tigerishan426@gmail.com>
2022-05-28 12:36:24 +0530

    Fixiing the pop up modal..

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -28,8 +28,8 @@ export const Api = () =>{
        return<>
         <div className="Card shadow-md" key={value.id}>
         <img className="UnsplashImage" src={value.urls.small} alt="image"/>
-        {isModalVisible && <CardModal key={value.id} setIsModalVisible={setModalVisible} id={value.id}  Name={value.user.name}/>}
         <button type="button" onClick={()=>{setModalVisible(true)}}class="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Details</button>
+        {isModalVisible && <CardModal key={value.id} setIsModalVisible={setModalVisible} id={value.id}  Name={value.user.name}/>}
         </div>
         </>
       })}
